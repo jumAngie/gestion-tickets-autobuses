79660b62cf67c40fe2deb437fb7bbbd70e7ef7d4
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-10-20 21:38:08 -0600

    Update TABLAS ACCESO Y TICK - CARLOS.sql

--- Base de Datos/TABLAS ACCESO Y TICK - CARLOS.sql
+++ Base de Datos/TABLAS ACCESO Y TICK - CARLOS.sql
@@ -65,13 +65,45 @@ CREATE TABLE Acce.tbRoles
 	CONSTRAINT UQ_Acce_tbRoles_rol_Descripcion	 	UNIQUE(rol_Descripcion),
 	
 	CONSTRAINT FK_Acce_tbUsuarios_usu_UsuarioCreacion_Acce_tbRoles_usu_ID		FOREIGN KEY (usu_UsuarioCreacion)		REFERENCES Acce.tbUsuarios (usu_ID),
-	CONSTRAINT FK_Acce_tbUsuarios_usu_UsuarioModificacion_Acce_tbRoles_usu_ID FOREIGN KEY (usu_UsuarioModificacion)	REFERENCES Acce.tbUsuarios (usu_ID),
+	CONSTRAINT FK_Acce_tbUsuarios_usu_UsuarioModificacion_Acce_tbRoles_usu_ID	FOREIGN KEY (usu_UsuarioModificacion)	REFERENCES Acce.tbUsuarios (usu_ID),
 );
 GO
 
 --**********************************************************--
 --*************** SCHEMA Tickets ***************************--
 --**********************************************************--
+
+---- -- TABLA MARCA -- -- --
+CREATE TABLE Tick.tbMarca
+(
+		mar_ID						INT				IDENTITY(1,1),
+		mar_Descripcion				NVARCHAR(500),
+
+		mar_UsuarioCreacion			INT							NOT NULL,
+		mar_UsuarioModificacion		INT							NOT NULL,
+		mar_FehaCreacion			DATETIME					NOT NULL,
+		mar_FechaModificacion		DATETIME					NOT NULL,
+		mar_Estado					BIT				DEFAULT		1,
+
+		CONSTRAINT PK_Tick_tbMarca_mar_ID			PRIMARY KEY(mar_ID),
+);
+GO
+
+---- -- TABLA MODELO -- -- --
+CREATE TABLE Tick.tbModelo
+(
+		mod_ID						INT				IDENTITY(1,1),
+		mod_Descripcion				NVARCHAR(500),
+
+		mod_UsuarioCreacion			INT							NOT NULL,
+		mod_UsuarioModificacion		INT							NOT NULL,
+		mod_FehaCreacion			DATETIME					NOT NULL,
+		mod_FechaModificacion		DATETIME					NOT NULL,
+		mod_Estado					BIT				DEFAULT		1,
+
+		CONSTRAINT PK_Tick_tbModelo_mod_ID			PRIMARY KEY(mod_ID),
+);
+GO
 
 -- -- -- TABLA AUTOBUSES -- -- --
 CREATE TABLE Tick.tbAutobuses
@@ -90,41 +122,8 @@ CREATE TABLE Tick.tbAutobuses
 		aut_Estado					BIT				DEFAULT		1,
 
 		CONSTRAINT PK_Tick_tbAutobuses_aut_ID		PRIMARY KEY(aut_ID),
+		CONSTRAINT FK_Tick_tbAutobuses_mar_Id		FOREIGN KEY (mar_ID) REFERENCES Tick.tbMarca (mar_ID),
+		CONSTRAINT FK_Tick_tbAutobuses_mod_Id		FOREIGN KEY (mod_ID) REFERENCES Tick.tbModelo (mod_ID)
 
-
-
-);
-GO
----- -- TABLA MARCA -- -- --
-CREATE TABLE Tick.tbMarca
-(
-		mar_ID						INT				IDENTITY(1,1),
-		mar_Descripcion				NVARCHAR(500),
-
-		mar_UsuarioCreacion			INT							NOT NULL,
-		mar_UsuarioModificacion		INT							NOT NULL,
-		mar_FehaCreacion			DATETIME					NOT NULL,
-		mar_FechaModificacion		DATETIME					NOT NULL,
-		mar_Estado					BIT				DEFAULT		1,
-
-		CONSTRAINT PK_Tick_tbMarca_mar_ID			PRIMARY KEY(mar_ID),
-		CONSTRAINT UQ_Tick_tbMarca_mar_Descripcion	UNIQUE(mar_Descripcion),
-);
-GO
-
----- -- TABLA MODELO -- -- --
-CREATE TABLE Tick.tbModelo
-(
-		mod_ID						INT				IDENTITY(1,1),
-		mod_Descripcion				NVARCHAR(500),
-
-		mod_UsuarioCreacion			INT							NOT NULL,
-		mod_UsuarioModificacion		INT							NOT NULL,
-		mod_FehaCreacion			DATETIME					NOT NULL,
-		mod_FechaModificacion		DATETIME					NOT NULL,
-		mod_Estado					BIT				DEFAULT		1,
-
-		CONSTRAINT PK_Tick_tbModelo_mod_ID			PRIMARY KEY(mod_ID),
-		CONSTRAINT UQ_Tick_tbModelo_mod_Descripcion	UNIQUE(mod_Descripcion),
 );
 GO
