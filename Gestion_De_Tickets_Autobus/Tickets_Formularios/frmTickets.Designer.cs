0a5335f5a40409140c22d9ef88ff03d7fba0ea8b
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-11-29 17:18:37 -0600

    Update frmTickets.Designer.cs

--- Gestion_De_Tickets_Autobus/Tickets_Formularios/frmTickets.Designer.cs
+++ Gestion_De_Tickets_Autobus/Tickets_Formularios/frmTickets.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(693, 414);
+            this.dataGridView1.TabIndex = 0;
             // 
             // frmTickets
             // 
@@ -36,12 +46,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(717, 438);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "frmTickets";
             this.Text = "fmrTickets";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
