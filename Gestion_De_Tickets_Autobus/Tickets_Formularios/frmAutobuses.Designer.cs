7cced284fa3d75bd43c0913dfca0e289f37991e4
★ Angie Campos ★ <125337384+jumAngie@users.noreply.github.com>
2024-12-10 11:01:17 -0600

    Update frmAutobuses.Designer.cs

--- Gestion_De_Tickets_Autobus/Tickets_Formularios/frmAutobuses.Designer.cs
+++ Gestion_De_Tickets_Autobus/Tickets_Formularios/frmAutobuses.Designer.cs
@@ -64,9 +64,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(364, 28);
+            this.label1.Location = new System.Drawing.Point(485, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.Size = new System.Drawing.Size(138, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingrese Matricula";
             // 
@@ -89,9 +90,11 @@
             this.groupBox1.Controls.Add(this.pnlAsientos);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(24, 28);
+            this.groupBox1.Location = new System.Drawing.Point(32, 34);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(775, 268);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1033, 330);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Información General";
@@ -101,9 +104,10 @@
             this.rbtEsVIP.AutoSize = true;
             this.rbtEsVIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rbtEsVIP.ForeColor = System.Drawing.Color.White;
-            this.rbtEsVIP.Location = new System.Drawing.Point(225, 172);
+            this.rbtEsVIP.Location = new System.Drawing.Point(300, 212);
+            this.rbtEsVIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbtEsVIP.Name = "rbtEsVIP";
-            this.rbtEsVIP.Size = new System.Drawing.Size(46, 20);
+            this.rbtEsVIP.Size = new System.Drawing.Size(56, 24);
             this.rbtEsVIP.TabIndex = 4;
             this.rbtEsVIP.TabStop = true;
             this.rbtEsVIP.Text = "VIP";
@@ -114,9 +118,10 @@
             this.rbtNormal.AutoSize = true;
             this.rbtNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rbtNormal.ForeColor = System.Drawing.Color.White;
-            this.rbtNormal.Location = new System.Drawing.Point(140, 172);
+            this.rbtNormal.Location = new System.Drawing.Point(187, 212);
+            this.rbtNormal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbtNormal.Name = "rbtNormal";
-            this.rbtNormal.Size = new System.Drawing.Size(69, 20);
+            this.rbtNormal.Size = new System.Drawing.Size(84, 24);
             this.rbtNormal.TabIndex = 4;
             this.rbtNormal.TabStop = true;
             this.rbtNormal.Text = "Normal";
@@ -127,48 +132,53 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(20, 174);
+            this.label5.Location = new System.Drawing.Point(27, 214);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 16);
+            this.label5.Size = new System.Drawing.Size(102, 20);
             this.label5.TabIndex = 27;
             this.label5.Text = "Exclusividad";
             // 
             // cbxMarcas
             // 
             this.cbxMarcas.FormattingEnabled = true;
-            this.cbxMarcas.Location = new System.Drawing.Point(22, 51);
+            this.cbxMarcas.Location = new System.Drawing.Point(29, 63);
+            this.cbxMarcas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbxMarcas.Name = "cbxMarcas";
-            this.cbxMarcas.Size = new System.Drawing.Size(260, 24);
+            this.cbxMarcas.Size = new System.Drawing.Size(345, 28);
             this.cbxMarcas.TabIndex = 30;
             this.cbxMarcas.SelectedIndexChanged += new System.EventHandler(this.cbxMarcas_SelectedIndexChanged);
             // 
             // cbxModelo
             // 
             this.cbxModelo.FormattingEnabled = true;
-            this.cbxModelo.Location = new System.Drawing.Point(23, 121);
+            this.cbxModelo.Location = new System.Drawing.Point(31, 149);
+            this.cbxModelo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbxModelo.Name = "cbxModelo";
-            this.cbxModelo.Size = new System.Drawing.Size(259, 24);
+            this.cbxModelo.Size = new System.Drawing.Size(344, 28);
             this.cbxModelo.TabIndex = 28;
             // 
             // pnlModelo
             // 
             this.pnlModelo.BackColor = System.Drawing.Color.DarkRed;
-            this.pnlModelo.Location = new System.Drawing.Point(23, 136);
+            this.pnlModelo.Location = new System.Drawing.Point(31, 167);
+            this.pnlModelo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlModelo.Name = "pnlModelo";
-            this.pnlModelo.Size = new System.Drawing.Size(259, 16);
+            this.pnlModelo.Size = new System.Drawing.Size(345, 20);
             this.pnlModelo.TabIndex = 22;
             this.pnlModelo.Visible = false;
             // 
             // numAsientos
             // 
-            this.numAsientos.Location = new System.Drawing.Point(367, 123);
+            this.numAsientos.Location = new System.Drawing.Point(489, 151);
+            this.numAsientos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numAsientos.Maximum = new decimal(new int[] {
             30,
             0,
             0,
             0});
             this.numAsientos.Name = "numAsientos";
-            this.numAsientos.Size = new System.Drawing.Size(248, 22);
+            this.numAsientos.Size = new System.Drawing.Size(331, 26);
             this.numAsientos.TabIndex = 11;
             // 
             // label2
@@ -176,9 +186,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(19, 28);
+            this.label2.Location = new System.Drawing.Point(25, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.Size = new System.Drawing.Size(56, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "Marca";
             // 
@@ -187,9 +198,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(19, 97);
+            this.label3.Location = new System.Drawing.Point(25, 119);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.Size = new System.Drawing.Size(63, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Modelo";
             // 
@@ -198,43 +210,48 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(364, 97);
+            this.label4.Location = new System.Drawing.Point(485, 119);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(129, 16);
+            this.label4.Size = new System.Drawing.Size(161, 20);
             this.label4.TabIndex = 8;
             this.label4.Text = "Numero de Asientos";
             // 
             // txtMatricula
             // 
-            this.txtMatricula.Location = new System.Drawing.Point(367, 51);
+            this.txtMatricula.Location = new System.Drawing.Point(489, 63);
+            this.txtMatricula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMatricula.Name = "txtMatricula";
-            this.txtMatricula.Size = new System.Drawing.Size(248, 22);
+            this.txtMatricula.Size = new System.Drawing.Size(329, 26);
             this.txtMatricula.TabIndex = 5;
             // 
             // pnlMarca
             // 
             this.pnlMarca.BackColor = System.Drawing.Color.DarkRed;
-            this.pnlMarca.Location = new System.Drawing.Point(23, 70);
+            this.pnlMarca.Location = new System.Drawing.Point(31, 86);
+            this.pnlMarca.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlMarca.Name = "pnlMarca";
-            this.pnlMarca.Size = new System.Drawing.Size(259, 12);
+            this.pnlMarca.Size = new System.Drawing.Size(345, 15);
             this.pnlMarca.TabIndex = 20;
             this.pnlMarca.Visible = false;
             // 
             // pnlMatricula
             // 
             this.pnlMatricula.BackColor = System.Drawing.Color.DarkRed;
-            this.pnlMatricula.Location = new System.Drawing.Point(367, 51);
+            this.pnlMatricula.Location = new System.Drawing.Point(489, 63);
+            this.pnlMatricula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlMatricula.Name = "pnlMatricula";
-            this.pnlMatricula.Size = new System.Drawing.Size(248, 29);
+            this.pnlMatricula.Size = new System.Drawing.Size(331, 36);
             this.pnlMatricula.TabIndex = 23;
             this.pnlMatricula.Visible = false;
             // 
             // pnlAsientos
             // 
             this.pnlAsientos.BackColor = System.Drawing.Color.DarkRed;
-            this.pnlAsientos.Location = new System.Drawing.Point(367, 123);
+            this.pnlAsientos.Location = new System.Drawing.Point(489, 151);
+            this.pnlAsientos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlAsientos.Name = "pnlAsientos";
-            this.pnlAsientos.Size = new System.Drawing.Size(248, 29);
+            this.pnlAsientos.Size = new System.Drawing.Size(331, 36);
             this.pnlAsientos.TabIndex = 21;
             this.pnlAsientos.Visible = false;
             // 
@@ -245,9 +262,10 @@
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(696, 308);
+            this.btnCancelar.Location = new System.Drawing.Point(928, 379);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(103, 31);
+            this.btnCancelar.Size = new System.Drawing.Size(137, 38);
             this.btnCancelar.TabIndex = 25;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
@@ -259,9 +277,10 @@
             this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGuardar.ForeColor = System.Drawing.Color.White;
-            this.btnGuardar.Location = new System.Drawing.Point(587, 308);
+            this.btnGuardar.Location = new System.Drawing.Point(783, 379);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(103, 31);
+            this.btnGuardar.Size = new System.Drawing.Size(137, 38);
             this.btnGuardar.TabIndex = 24;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = false;
@@ -270,11 +289,11 @@
             // dgAutobuses
             // 
             this.dgAutobuses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgAutobuses.Location = new System.Drawing.Point(24, 348);
-            this.dgAutobuses.Margin = new System.Windows.Forms.Padding(2);
+            this.dgAutobuses.Location = new System.Drawing.Point(32, 428);
+            this.dgAutobuses.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgAutobuses.Name = "dgAutobuses";
             this.dgAutobuses.RowHeadersWidth = 51;
-            this.dgAutobuses.Size = new System.Drawing.Size(775, 144);
+            this.dgAutobuses.Size = new System.Drawing.Size(1033, 249);
             this.dgAutobuses.TabIndex = 11;
             this.dgAutobuses.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgAutobuses_CellClick);
             // 
@@ -284,9 +303,10 @@
             this.lblAdvertencia.BackColor = System.Drawing.Color.DarkRed;
             this.lblAdvertencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAdvertencia.ForeColor = System.Drawing.Color.Snow;
-            this.lblAdvertencia.Location = new System.Drawing.Point(576, 9);
+            this.lblAdvertencia.Location = new System.Drawing.Point(768, 11);
+            this.lblAdvertencia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAdvertencia.Name = "lblAdvertencia";
-            this.lblAdvertencia.Size = new System.Drawing.Size(223, 16);
+            this.lblAdvertencia.Size = new System.Drawing.Size(273, 20);
             this.lblAdvertencia.TabIndex = 13;
             this.lblAdvertencia.Text = "¡Debe llenar todos los campos!";
             this.lblAdvertencia.Visible = false;
@@ -298,17 +318,19 @@
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.txtBuscar);
-            this.panel1.Location = new System.Drawing.Point(24, 309);
+            this.panel1.Location = new System.Drawing.Point(32, 380);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(357, 30);
+            this.panel1.Size = new System.Drawing.Size(475, 36);
             this.panel1.TabIndex = 33;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Gestion_De_Tickets_Autobus.Properties.Resources.buscarcolorizado;
-            this.pictureBox1.Location = new System.Drawing.Point(328, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(437, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(24, 22);
+            this.pictureBox1.Size = new System.Drawing.Size(32, 27);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 32;
             this.pictureBox1.TabStop = false;
@@ -316,9 +338,10 @@
             // txtBuscar
             // 
             this.txtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBuscar.Location = new System.Drawing.Point(3, 3);
+            this.txtBuscar.Location = new System.Drawing.Point(4, 4);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(319, 22);
+            this.txtBuscar.Size = new System.Drawing.Size(424, 26);
             this.txtBuscar.TabIndex = 1;
             this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
             // 
@@ -328,9 +351,10 @@
             this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditar.ForeColor = System.Drawing.Color.White;
-            this.btnEditar.Location = new System.Drawing.Point(587, 308);
+            this.btnEditar.Location = new System.Drawing.Point(783, 379);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(103, 31);
+            this.btnEditar.Size = new System.Drawing.Size(137, 38);
             this.btnEditar.TabIndex = 35;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = false;
@@ -339,10 +363,10 @@
             // 
             // frmAutobuses
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(819, 503);
+            this.ClientSize = new System.Drawing.Size(1092, 688);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnCancelar);
@@ -350,6 +374,7 @@
             this.Controls.Add(this.dgAutobuses);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblAdvertencia);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmAutobuses";
             this.Text = "fmrAutobuses";
             this.Load += new System.EventHandler(this.frmAutobuses_Load);
